d79639d4c8916e6aa09f2f49efc7af7df553fd91
连成的轻薄本小新 <741693131@qq.com>
2023-10-09 20:21:33 +0800

    图片和方程的TensorFlowboard上传完成

---
+++ main.py
@@ -0,0 +1,26 @@
+from torch.utils.tensorboard import SummaryWriter
+import tensorflow
+from PIL import Image
+import cv2
+import numpy as np
+
+# 创建一个名为 "logs" 的 TensorBoard 日志写入器
+writer = SummaryWriter("logs")
+
+image_path="pic (2).jpg"
+img=Image.open(image_path)
+print(type(img))
+img_array=np.array(img)
+print(type(img_array))
+
+writer.add_image("test",img_array,2,dataformats="HWC")
+
+# 循环从 0 到 99，将数据写入 TensorBoard
+for i in range(100):
+    # 使用 add_scalar 方法将名为 "y=x" 的标量数据写入 TensorBoard
+    writer.add_scalar('y=x', i, i)
+
+# 关闭 TensorBoard 写入器
+writer.close()
+
+#  使用tensorboard - -logdir = logs运行
